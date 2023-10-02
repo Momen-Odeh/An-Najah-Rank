988c17979851322c60228101009a5e1351380c84
Momen-Odeh <92532348+Momen-Odeh@users.noreply.github.com>
2023-10-03 00:13:16 +0300

    send email API

---
+++ DataBase/FlaskSetUp.py
@@ -0,0 +1,8 @@
+from flask import Flask
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "tsfyguaistyatuis589566875623568956"
+app.config['MAIL_SERVER'] = "smtp.googlemail.com"
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = "an.najah.rank@gmail.com"
+app.config['MAIL_PASSWORD'] = "rfjk robs wklz svvc"
--- DataBase/main.py
+++ DataBase/main.py
@@ -1,19 +1,21 @@
-from flask import Flask
-from dataBaseConnection import connect_to_database
-from dataBaseConnection import execute_query
-from dataBaseConnection import fetch_results
-from dataBaseConnection import close_connection
-app = Flask(__name__)
-host = 'localhost'
-user = 'root'
-password = 'Noor@2002'
-database = 'an-najah rank'
-connection = connect_to_database(host, user, password, database)
-query = 'SELECT * FROM user'
-cursor = execute_query(connection, query)
-results = fetch_results(cursor)
-close_connection(cursor,connection)
-for user in results:
-    print(user)
+from flask import request, jsonify
+from EmailAuth.emailConnection import sendEmail
+from FlaskSetUp import app
+from MySQL_SetUp import connection
+
+@app.route('/sendEmail', methods=['POST'])
+def mailAPI():
+    if request.is_json:
+        try:
+            data = request.get_json()
+            email = data.get('email')
+            title = data.get('title')
+            body = "reset password body"
+            return sendEmail(email,title,body)
+        except Exception as e:
+            return jsonify({"error": "Invalid JSON data"}), 400
+    else:
+        return jsonify({"error": "Request body must contain JSON data"}), 400
+
 if __name__ == "__main__":
     app.run(debug=True)
