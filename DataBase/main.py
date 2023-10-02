8e1e6912f77845432eebd6c09d07f3a2ae29adcb
Noor Aldeen <anooraldeen9@gmail.com>
2023-10-03 00:18:56 +0300

    finish registration api

--- DataBase/main.py
+++ DataBase/main.py
@@ -1,19 +1,17 @@
-from flask import Flask
+from flask import Flask, request
 from dataBaseConnection import connect_to_database
-from dataBaseConnection import execute_query
-from dataBaseConnection import fetch_results
-from dataBaseConnection import close_connection
+from APIs.registration import register_user
 app = Flask(__name__)
 host = 'localhost'
 user = 'root'
 password = 'Noor@2002'
 database = 'an-najah rank'
 connection = connect_to_database(host, user, password, database)
-query = 'SELECT * FROM user'
-cursor = execute_query(connection, query)
-results = fetch_results(cursor)
-close_connection(cursor,connection)
-for user in results:
-    print(user)
+@app.route('/register', methods=['POST'])
+def register():
+    request_data = request.json
+    return register_user(connection, request_data)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
