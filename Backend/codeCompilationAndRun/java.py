1c1c1f52ad704fdc122a6aa882f21f87c5309ece
Noor Aldeen <anooraldeen9@gmail.com>
2023-10-29 22:11:34 +0200

    finish Input stream for java code

--- Backend/codeCompilationAndRun/java.py
+++ Backend/codeCompilationAndRun/java.py
@@ -18,24 +18,27 @@ def compileJavaCode(javaFilePath):
 
 
 # ---------------------------------------------------------------------------------
-def runJavaCode(folderPath,java_class_name):
+def runJavaCode(folderPath, java_class_name, input_data):
     try:
-        result = subprocess.run(['java', '-cp',folderPath,java_class_name], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-        if result.returncode == 0:
-            return result.stdout, None  # No error occurred, return stdout
+        result = None
+        if input_data:
+            result = subprocess.run(['java', '-cp', folderPath, java_class_name], input=input_data, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
         else:
-            return None, result.stderr  # An error occurred, return stderr
+            result = subprocess.run(['java', '-cp',folderPath,java_class_name], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        if result.returncode == 0:
+            return result.stdout, None
+        else:
+            return None, result.stderr
     except subprocess.CalledProcessError as e:
         return None, e.stderr
 
-def compileAndRunJavaCode(code):
+def compileAndRunJavaCode(code, input_data):
     try:
-        codePath = saveCodeToFile("javaTest","java","code/Momen",code)
+        codePath = saveCodeToFile("javaTest", "java", "code/Momen", code)
         folderPath = os.path.dirname(codePath)
-        success,std = compileJavaCode(codePath)
-        # *********************
+        success, std = compileJavaCode(codePath)
         if success:
-            stdout, stderr = runJavaCode(folderPath,"Main")
+            stdout, stderr = runJavaCode(folderPath, "Main", input_data)
             if stdout is not None:
                 return jsonify({'output': stdout})
             else:
@@ -43,6 +46,7 @@ def compileAndRunJavaCode(code):
         else:
             return jsonify({'error': 'Compile time error', 'stderr': std}), 400
     except Exception as e:
-        return False, str(e)  # Compilation failed due to an exception
+        return jsonify({'error': str(e)}), 500
+
 
 
