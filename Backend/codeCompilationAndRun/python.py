87e9ff8b920e8d8f310fceb328e1ebd2dce174e8
Noor Aldeen <anooraldeen9@gmail.com>
2023-10-29 22:12:56 +0200

    finish Input stream for python code

--- Backend/codeCompilationAndRun/python.py
+++ Backend/codeCompilationAndRun/python.py
@@ -1,11 +1,14 @@
 from codeCompilationAndRun.storeCodeFile import saveCodeToFile
 import subprocess
 from flask import jsonify
-def configPythonCode(pythonFilePath):
+def configPythonCode(pythonFilePath, input_data=None):
     try:
-        result = subprocess.run(['python', pythonFilePath], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        if input_data is not None:
+            result = subprocess.run(['python', pythonFilePath], input=input_data, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        else:
+            result = subprocess.run(['python', pythonFilePath], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
         if result.returncode == 0:
-            return True,result.stdout
+            return True, result.stdout
         else:
             error_message = result.stderr
             return False, error_message
@@ -13,13 +16,13 @@ def configPythonCode(pythonFilePath):
     except Exception as e:
         return False, str(e)
 
-def runPythonCode(code):
+def runPythonCode(code, input_data=None):
     try:
-        codePath = saveCodeToFile("pythonTest", "py","code/NoorAldeen", code)
-        success, std = configPythonCode(codePath)
+        codePath = saveCodeToFile("pythonTest", "py", "code/NoorAldeen", code)
+        success, std = configPythonCode(codePath, input_data)
         if success:
-                return jsonify({'output': std})
+            return jsonify({'output': std})
         else:
             return jsonify({'error': 'Error', 'stderr': std}), 400
     except Exception as e:
-        return str(e)
+        return jsonify({'error': str(e)}), 500
