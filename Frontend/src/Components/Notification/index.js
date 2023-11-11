4fea45483cec8685a455be8b2116ed87ab4a922d
Noor Aldeen <anooraldeen9@gmail.com>
2023-11-11 10:25:03 +0200

    finish notification in frontend side

---
+++ Frontend/src/Components/Notification/index.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import io from "socket.io-client";
+
+const socket = io("http://localhost:5000");
+
+const App = () => {
+  const [notifications, setNotifications] = useState([]);
+
+  socket.on("notification", (notification) => {
+    setNotifications([...notifications, notification]);
+  });
+
+  const handleAdd = () => {
+    socket.emit("add_user");
+  };
+
+  return (
+    <div>
+      <button onClick={handleAdd}>Add</button>
+
+      {notifications.map((notification) => (
+        <div>
+          <h3>{notification.title}</h3>
+          <p>{notification.body}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default App;
