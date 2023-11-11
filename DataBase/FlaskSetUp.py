6a7a30b61a401b09e65ee6ee721174577fdc4942
Noor Aldeen <anooraldeen9@gmail.com>
2023-11-11 10:22:31 +0200

    setup socket io

--- DataBase/FlaskSetUp.py
+++ DataBase/FlaskSetUp.py
@@ -1,5 +1,8 @@
 from flask import Flask
+from flask_socketio import SocketIO
+
 app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins="*")#http://localhost:3000
 app.config['SECRET_KEY'] = "tsfyguaistyatuis589566875623568956"
 app.config['MAIL_SERVER'] = "smtp.googlemail.com"
 app.config['MAIL_PORT'] = 587
