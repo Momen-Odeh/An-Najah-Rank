f67e094c82b12dbb7e3973f9a00a344d6944e629
Noor Aldeen <anooraldeen9@gmail.com>
2023-12-20 22:01:49 +0200

    change port numbers

--- Backend/main.py
+++ Backend/main.py
@@ -68,4 +68,4 @@ def applayRE():
         return jsonify({"error": "Request body must contain JSON data"}), 400
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    app.run(debug=True)
--- Frontend/src/Utils/BaseURI.js
+++ Frontend/src/Utils/BaseURI.js
@@ -1,2 +1,2 @@
-export const BaseURI = "http://localhost:5000";
+export const BaseURI = "http://localhost:5001";
 export default BaseURI;
