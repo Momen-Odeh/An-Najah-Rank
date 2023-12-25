db8c9bfa08545c00ea1fb7520c5733fac4ce1c45
Noor Aldeen Abu Shehadeh <102482887+NoorAldeenAbuShehadeh@users.noreply.github.com>
2023-12-25 20:58:07 +0200

    Update BaseURI.js

--- Frontend/src/Utils/BaseURI.js
+++ Frontend/src/Utils/BaseURI.js
@@ -1,3 +1,3 @@
-export const BaseURI = "http://172.18.0.4:5001";
-export const SocketURI = "http://172.18.0.2:5004";
+export const BaseURI = "http://localhost:5001";
+export const SocketURI = "http://localhost:5004";
 export default BaseURI;
