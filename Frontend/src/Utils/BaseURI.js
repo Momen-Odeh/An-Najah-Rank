688ff363a38a2cf4a3385c007f0bf23ffdaf2e40
Noor Aldeen <anooraldeen9@gmail.com>
2023-12-28 01:43:44 +0200

    update base URI in frontend

--- Frontend/src/Utils/BaseURI.js
+++ Frontend/src/Utils/BaseURI.js
@@ -1,3 +1,3 @@
-export const BaseURI = "http://localhost:5001";
-export const SocketURI = "http://localhost:5004";
+export const BaseURI = "http://51.20.98.159:5001";
+export const SocketURI = "http://51.20.98.159:5004";
 export default BaseURI;
