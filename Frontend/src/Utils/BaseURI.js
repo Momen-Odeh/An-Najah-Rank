e2fafcbe5241896a6bbbea1d117d34faabb15551
Momen-Odeh <92532348+Momen-Odeh@users.noreply.github.com>
2023-12-29 11:41:18 +0200

    update some info

--- Frontend/src/Utils/BaseURI.js
+++ Frontend/src/Utils/BaseURI.js
@@ -1,3 +1,3 @@
-export const BaseURI = "http://database/:5001";
-export const SocketURI = "http://socketio/:5004";
+export const BaseURI = "http://localhost:5001";
+export const SocketURI = "http://localhost:5004";
 export default BaseURI;
