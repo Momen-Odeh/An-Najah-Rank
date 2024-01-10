662164d5b37d840210eb4beb0a7f48dc6db1750b
Noor Aldeen <anooraldeen9@gmail.com>
2024-01-10 18:39:43 +0200

    update sample explanation

--- Frontend/src/Components/SampleContainer/index.js
+++ Frontend/src/Components/SampleContainer/index.js
@@ -3,7 +3,14 @@ import useStyles from "./style";
 
 const SampleContainer = ({ data, backgroundColor = "#f4faff", border }) => {
   const classes = useStyles({ backgroundColor, border });
-  return <pre className={classes.Sample}> {data}</pre>;
+  return (
+    <pre
+      className={classes.Sample}
+      dangerouslySetInnerHTML={{
+        __html: data,
+      }}
+    />
+  );
 };
 
 export default SampleContainer;
