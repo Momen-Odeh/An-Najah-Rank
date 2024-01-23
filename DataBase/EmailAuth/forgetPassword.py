a0cbadc00c1926c00444a4fb085056567fb44789
Momen-Odeh <92532348+Momen-Odeh@users.noreply.github.com>
2024-01-24 00:50:03 +0200

    active resend verfication code

--- DataBase/EmailAuth/forgetPassword.py
+++ DataBase/EmailAuth/forgetPassword.py
@@ -2,10 +2,8 @@ from flask import request, jsonify
 from FlaskSetUp import app
 from MySQL_SetUp import connection
 from dataBaseConnection import update_data,insert_data,execute_query,fetch_results
-from  EmailAuth.createVerificationCode import genrateRandomCode
-from  EmailAuth.emailConnection import sendEmail
+from  EmailAuth.createVerificationCode import sendForgetEmail
 
-import datetime
 @app.route('/forgetPassword', methods=['POST'])
 def forgetPassword():
         try:
@@ -21,26 +19,5 @@ def forgetPassword():
         except Exception as e:
             return jsonify({"error": f"{e}"}), 400
 
-def sendForgetEmail(email):
-    code = genrateRandomCode()
-    TTL = datetime.datetime.now() + datetime.timedelta(minutes=20)
-#
-    try:
-        result = insert_data(connection,'code_verification', ['email','code','TTL'],(email,code,TTL))
-        print(result[1])
-        if (result[1] == 409):
-            print("Update")
-            update_data(connection, 'code_verification', ['code', 'TTL'], (code, TTL),f"(`email` = '{email}')")
 
-        msg = f"""
-        <p>Hello,<br> We just need to verify your email address before you can reset your password. <br>
-        Reset your password by entering this code:<br>
-        <h2><strong>{code}</strong></h2>
-        Thanks!<br>An Najah Rank team</p>
-        """
-
-        return(sendEmail(email,"An Najah Rank Reset Password Code",msg))
-
-    except Exception as e:
-        print(f"Error while registering user: {e}")
 #
