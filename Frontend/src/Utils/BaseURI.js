f8b4a28d8c653c6855c89b30bebb13f2da16f4b1
Noor Aldeen <anooraldeen9@gmail.com>
2024-01-26 16:00:15 +0200

    update build files

--- Frontend/src/Utils/BaseURI.js
+++ Frontend/src/Utils/BaseURI.js
@@ -1,3 +1,3 @@
-// export const BaseURI = "http://16.171.229.146:5001";
-export const BaseURI = "http://localhost:5001";
+export const BaseURI = "http://16.171.229.146:5001";
+// export const BaseURI = "http://localhost:5001";
 export default BaseURI;
